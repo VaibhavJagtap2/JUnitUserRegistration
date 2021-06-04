7974f1ae83c66ea05fd9e61ff17770b330f79d48
VaibhavJagtap2 <vaibhavjagtap03.vj@gmail.com>
2021-06-04 21:33:15 +0530

    Add:Check condition for Password

--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -5,101 +5,172 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void givenFirstName_WhenStartWithCapitalLetter_ShouldReturn_True(){
+    //when the name starts with a capital letter
+    public void givenFirstName_ShouldReturn_True() {
         UserMain userMain = new UserMain();
         boolean result = userMain.FirstName("Vaibhav");
-        Assert.assertEquals(result,true);
+        Assert.assertEquals(true, result);
     }
     @Test
-    public void givenFirstName_WhenNotStartWithCapitalLetter_ShouldReturn_False(){
+    //when a name starts with a small letter
+    public void givenFirstName_ShouldReturn_False() {
         UserMain userMain = new UserMain();
         boolean result = userMain.FirstName("vaibhav");
-        Assert.assertEquals(result,false);
+        Assert.assertEquals(false, result);
     }
+    //when a name has 3 or more letters
     @Test
-    public void givenFirstName_WhenHaveMinimumThreeLetters_ShouldReturn_True(){
+    public void givenFirstName_MinimumThreeLetters_ShouldReturn_True() {
         UserMain userMain = new UserMain();
         boolean result = userMain.FirstName("Vai");
-        Assert.assertEquals(result,true);
+        Assert.assertEquals(true, result);
     }
     @Test
-    public void  givenFirstName_WhenHaveMinimumThreeLetters_ShouldReturn_False(){
+    //when a name does not contain 3 minimum letters
+    public void givenFirstName_NotHaveMinimumThreeLetters_ShouldReturn_False() {
         UserMain userMain = new UserMain();
         boolean result = userMain.FirstName("Va");
-        Assert.assertEquals(result,false);
+        Assert.assertEquals(false, result);
     }
+
     @Test
-    public void givenLastName_WhenWithCapitalShouldReturn_True(){
+    public void givenLastName_WhenLastNameStartsWithCapitalLetter_ShouldReturn_True() {
         UserMain userMain = new UserMain();
         boolean result = userMain.LastName("Jagtap");
-        Assert.assertEquals(result,true);
+        Assert.assertEquals(true, result);
     }
+
     @Test
-    public void givenLastName_WhenNotWithCapitalShouldReturn_False(){
+    public void givenLastName_WhenLastNameNotStartsWithCapitalLetter_ShouldReturn_False() {
         UserMain userMain = new UserMain();
         boolean result = userMain.LastName("jagtap");
-        Assert.assertEquals(result,false);
+        Assert.assertEquals(false, result);
     }
+
     @Test
-    public void givenLastName_WhenHaveMinimumThreeLetterStartWithCapital_ShouldReturn_True(){
+    public void givenLastName_WhenLastNameHaveMinimumThreeLetter_ShouldReturn_True() {
         UserMain userMain = new UserMain();
         boolean result = userMain.LastName("Jag");
-        Assert.assertEquals(result,true);
+        Assert.assertEquals(true, result);
     }
-    @Test
-    public void givenLastName_WhenHaveMinimumThreeLetterNotStartWithCapital_ShouldReturn_False(){
-        UserMain userMain = new UserMain();
-        boolean result = userMain.LastName("jag");
-        Assert.assertEquals(result,false);
-    }
-    @Test
-    public void givenEmail_WhenStartWithSmallLetter_ShouldReturn_True(){
-        UserMain userMain = new UserMain();
-        boolean result = userMain.Email("abc54@gmail.com");
-        Assert.assertEquals(result,true);
-    }
-    @Test
-    public void givenEmail_WhenStartWithCapitalLetter_ShouldReturn_False(){
-        UserMain userMain = new UserMain();
-        boolean result = userMain.Email("Abc53@gmail.com");
-        Assert.assertEquals(result,false);
-    }
-    @Test
-    public void givenEmail_WhenEndsWithcomOrin_ShouldReturn_True(){
-      UserMain userMain = new UserMain();
-      boolean result = userMain.Email("abc54@yahoo.in");
-      Assert.assertEquals(result,true);
-    }
-    @Test
-    public void givenEmail_WhenNotEndsWithcomOrin_ShouldReturn_False(){
-        UserMain userMain = new UserMain();
-        boolean result = userMain.Email("Abc.53@yahoo.in");
-        Assert.assertEquals(result, false);
 
+    @Test
+    public void givenLastName_WhenLastNameNotHaveMinimumThreeLetter_ShouldReturn_False() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.LastName("ja");
+        Assert.assertEquals(false, result);
     }
+
+    @Test
+    public void givenEmail_WhenEmailStartsWithSmallLetter_ShouldReturn_True() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Email("abc45@gmail.com");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail_WhenEmailNotStartsWithSmallLetter_ShouldReturn_False() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Email("Abc@gmail.com");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenEmail_WhenEmailEndsWithComOrIn_ShouldReturn_True() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Email("abc111@yahoo.com");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail_WhenEmailNotEndsWithComOrIn_ShouldReturn_False() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Email("Abc@gmail.kjnk");
+        Assert.assertEquals(false, result);
+    }
+
     @Test
     public void givenPhoneNumber_WhenPhoneNumberIsFollowedByCountryCode_True() {
         UserMain userMain = new UserMain();
-        boolean result = userMain.Phone("91 8794385484");
-        Assert.assertEquals(result,true);
+        boolean result = userMain.Phone("91 9604445258");
+        Assert.assertEquals(true, result);
     }
+
     @Test
-    public void givenPhoneNumber_IsNotFollowedByCountryCode_ShouldReturn_False(){
+    public void givenPhoneNumber_WhenPhoneNumberIsNotFollowedByCountryCode_ShouldReturn_False() {
         UserMain userMain = new UserMain();
-        boolean result = userMain.Phone("892393903");
-        Assert.assertEquals(result,false);
+        boolean result = userMain.Phone("8105215414");
+        Assert.assertEquals(false, result);
     }
+
     @Test
     public void givenPhoneNumber_WhenPhoneNumberIsTenDigit_True() {
         UserMain userMain = new UserMain();
-        boolean result = userMain.Phone("91 7868334363");
-        Assert.assertEquals(result,true);
+        boolean result = userMain.Phone("91 9604445258");
+        Assert.assertEquals(true, result);
     }
+
     @Test
     public void givenPhoneNumber_WhenPhoneNumberIsNotTenDigit_ShouldReturn_False() {
         UserMain userMain = new UserMain();
-        boolean result = userMain.Phone("91 812399423");
-        Assert.assertEquals(result,false);
+        boolean result = userMain.Phone("91 8105215");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenPassword_WhenPasswordIsMinimumEightDigit_ShouldReturn_True() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Password("Vaibhav43#2");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPassword_WhenPasswordIsNotMinimumEightDigit_ShouldReturn_False() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Password("vaibns");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenPassword_WhenPasswordHaveAtleastOneCapitalLetter_ShouldReturn_True() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Password("Vaibhav@345");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPassword_WhenPasswordNotHaveAtleastOneCapitalLetter_ShouldReturn_False() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Password("vsvsai@");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenPassword_WhenPasswordHaveAtleastOneNumericNumber_ShouldReturn_True() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Password("Vaibhav2@3");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPassword_WhenPasswordNotHaveAtleastOneNumericNumber_ShouldReturn_False() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Password("adased@");
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void givenPassword_WhenPasswordHaveAtleastOneSpeacialCharacter_ShouldReturn_True() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Password("Vaibahv234$");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPassword_WhenPasswordNotHaveAtleastOneSpeacialCharacter_ShouldReturn_False() {
+        UserMain userMain = new UserMain();
+        boolean result = userMain.Password("vaibahv123");
+        Assert.assertEquals(false, result);
     }
 }
 
